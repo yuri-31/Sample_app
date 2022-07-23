468453e63582c67c3f4b8521eba4e573374d46fc
YuriNakano <yuriinlethbridge@outlook.com>
2022-07-23 19:27:04 +0000

    [Finish]3-2

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,7 +1,14 @@
 class ListsController < ApplicationController
   def new
+    @list = List.new
   end
 
+  def create
+    list = List.new(list_params)
+    list.save
+    redirect_to '/top'
+  end
+  
   def index
   end
 
@@ -9,5 +16,10 @@ class ListsController < ApplicationController
   end
 
   def edit
+  end
+  
+  private
+  def list_params
+    params.require(:list).permit(:title, :body)
   end
 end
